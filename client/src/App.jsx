43a45eaa5de5c3e97ef9c62e98255c7dec6fbfec
mansirathor27 <mansirathor575@gmail.com>
2026-03-14 10:23:11 +0530

    Add student and teacher management functionality with CRUD operations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,18 @@ import LoginPage from "./pages/auth/LoginPage";
 import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage";
 import ResetPasswordPage from "./pages/auth/ResetPasswordPage";
 
+// Dashboard Layouts
+import DashboardLayout from "./components/layout/DashboardLayout";
 
+
+import ManageStudents from "./pages/admin/ManageStudents";
+import ManageTeachers from "./pages/admin/ManageTeachers";
 
 import { useDispatch, useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import { Loader } from "lucide-react";
 import { getUser } from "./store/slices/authSlice";
+import { getAllUsers } from "./store/slices/adminSlice";
 
 const App = () => {
 
@@ -21,6 +27,35 @@ const App = () => {
   useEffect(()=>{
     dispatch(getUser());
   },[dispatch]);
+
+  useEffect(()=>{
+    if(authUser?.role === "Admin"){
+      dispatch(getAllUsers());
+    }
+  }, [authUser])
+
+ const ProtectedRoute = ({ children, allowedRoles }) => {
+  if (!authUser) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (
+    allowedRoles?.length &&
+    authUser?.role &&
+    !allowedRoles.includes(authUser.role)
+  ) {
+    const redirectPath =
+      authUser.role === "Admin"
+        ? "/admin"
+        : authUser.role === "Teacher"
+        ? "/teacher"
+        : "/student";
+
+    return <Navigate to={redirectPath} replace />;
+  }
+
+  return children;
+};
 
   if(isCheckingAuth && !authUser){
     return (
@@ -32,10 +67,23 @@ const App = () => {
   return (
     <BrowserRouter>
     <Routes>
-      {/* <Route path="/" element={<Home />} /> */}
+      {/* Auth Routes */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
       <Route path="/reset-password" element={<ResetPasswordPage />} />
+
+      {/* Admin Routes */}
+      <Route path ="/admin" element={
+        <ProtectedRoute allowedRoles={["Admin"]}>
+          <DashboardLayout userRole={"Admin"}/>
+        </ProtectedRoute>
+      }
+      >
+        
+        <Route path="students" element={<ManageStudents/>}/>
+        <Route path="teachers" element={<ManageTeachers/>}/>
+        
+      </Route>
     </Routes>
     <ToastContainer theme="dark"/>
     </BrowserRouter>
