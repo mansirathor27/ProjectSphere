94199b57b80d507d9eeffcea546cee4ef6138914
mansirathor27 <mansirathor575@gmail.com>
2026-03-22 09:28:54 +0530

    Added route file for deadlines page of admin

---
+++ server/router/deadlineRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+import { isAuthenticated, isAuthorized } from '../middlewares/authMiddleware.js';
+import { createDeadline } from '../controllers/deadlineController.js';
+const router = express.Router();
+
+router.post("/create-deadline/:id", isAuthenticated, isAuthorized("Admin"), createDeadline);
+
+
+
+export default router;
