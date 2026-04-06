17535507aac98a86f77574297a00dffd7a369c1f
mansirathor27 <mansirathor575@gmail.com>
2026-04-06 23:04:00 +0530

    fix cors

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import {config} from "dotenv";
 import cookieParser from 'cookie-parser';
 import { errorMiddleware } from './middlewares/error.js';
-// import errorMiddleware from './middlewares/error.js';
 import authRouter from "./router/userRoutes.js";
 import adminRouter from "./router/adminRoutes.js";
 import studentRouter from "./router/studentRoutes.js";
@@ -25,11 +24,13 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors({
-    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-    })
-);
+  origin: [
+    "http://localhost:5173",
+    "https://projecttsphere.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 
 const uploadsDir = path.join(__dirname, "uploads");
 const tempDir = path.join(__dirname, "temp");
