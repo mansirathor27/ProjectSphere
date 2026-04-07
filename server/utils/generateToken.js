bb48361ada1e5ad1cd5e73d147fd932aac59c168
mansirathor27 <mansirathor575@gmail.com>
2026-04-07 15:35:23 +0530

    Fix cookie expire issue final

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,7 +1,7 @@
 export const generateToken = (user, statusCode, message, res)=>{
     const token = user.generateToken();
     res.status(statusCode).cookie("token",token, {
-        expires: new Date(Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000),
+        expires: new Date(Date.now() + Number(process.env.COOKIE_EXPIRE || 7) * 24 * 60 * 60 * 1000),
         httpOnly: true,
     })
     .json({
